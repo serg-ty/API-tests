a1d5296605591049641c7a57be7a924f3de830a6
Терлецкий Сергей <terletskii.sergei@otr.ru>
2022-02-28 15:56:22 +0300

    Refactored test methods according to code review

--- src/test/java/com/reqres/tests/api/TestsAPI.java
+++ src/test/java/com/reqres/tests/api/TestsAPI.java
@@ -11,28 +11,32 @@ import java.util.List;
 public class TestsAPI extends BaseTest {
 
     @Test
-    @DisplayName("Проверка почты пользователя на первой странице ответа")
-    void checkUserMailOnPage1() {
+    @DisplayName("Проверка почты пользователя по полному совпадению")
+    void checkUserMailByCompleteMatch() {
 
-        List<User> users = Steps.getUsersByPage(requestSpec, 1);
+        List<User> users = Steps.findUserByAllPages(requestSpec,
+                u -> u.getFirstName().equals("George") && u.getLastName().equals("Bluth"));
+        String userMail = null;
+        if (!users.isEmpty())
+            userMail = users.get(0).getEmail();
 
-        String userMail = users.stream()
-                .filter(u -> u.getFirstName().equals("George") && u.getLastName().equals("Bluth"))
-                .findAny().orElseThrow().getEmail();
-
-        Assertions.assertEquals("george.bluth@reqres.in", userMail);
+        Assertions.assertNotEquals(0, users.size(), "Не нашли пользователя с заданным фильтром");
+        Assertions.assertEquals("george.bluth@reqres.in", userMail,
+                "Почта пользователя не совпадает с ожидаемым значением");
     }
 
     @Test
-    @DisplayName("Проверка почты пользователя на второй странице ответа")
-    void checkUserMailOnPage2() {
+    @DisplayName("Проверка почты пользователя по частичному совпадению")
+    void checkUserMailByPartialMatch() {
 
-        List<User> users = Steps.getUsersByPage(requestSpec, 2);
+        List<User> users = Steps.findUserByAllPages(requestSpec,
+                u -> u.getFirstName().equals("Michael") && u.getLastName().equals("Lawson"));
+        String userMail = null;
+        if (!users.isEmpty())
+            userMail = users.get(0).getEmail();
 
-        String userMail = users.stream()
-                .filter(u -> u.getFirstName().equals("Michael") && u.getLastName().equals("Lawson"))
-                .findAny().orElseThrow().getEmail();
-
-        Assertions.assertEquals("michael.lawson@reqres.in", userMail);
+        Assertions.assertNotEquals(0, users.size(), "Не нашли пользователя с заданным фильтром");
+        Assertions.assertTrue(userMail.contains("michael.lawson@reqres"),
+                "Почта пользователя не совпадает с ожидаемым значением");
     }
 }
